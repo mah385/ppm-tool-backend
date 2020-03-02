4333f5a97ce90f5d110e7b85237ec73491fb8e3e
mah385 <santhosh.p@gofrugal.com>
2020-03-02 18:58:55 +0530

    enabled CORS and also added TRACE level logging for Hibernate BasicBinder

--- src/main/java/com/ppmtool/controller/ProjectController.java
+++ src/main/java/com/ppmtool/controller/ProjectController.java
@@ -16,6 +16,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping(value = "/api")
+@CrossOrigin(origins = {"http://localhost:3000"})
 public class ProjectController {
 
     private final ProjectService projectService;
