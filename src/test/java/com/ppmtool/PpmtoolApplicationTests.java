bcc99a0ba80debfa6091b4f3a48d8a1fdf390d94
mah385 <mdarifhussain385@gmail.com>
2020-09-06 23:06:05 +0530

    Migrated to Spring Boot v2.3.3

--- src/test/java/com/ppmtool/PpmtoolApplicationTests.java
+++ src/test/java/com/ppmtool/PpmtoolApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PpmToolBackendApplicationTests {
+class PpmtoolApplicationTests {
 
     @Test
     void contextLoads() {
